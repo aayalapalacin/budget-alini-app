aa3e53ce254f7c5ce4a249d71a2ff6d9e03ea0b4
Alex <alexander.ayala.palacin@gmail.com>
2025-05-11 23:46:24 -0400

    added back unathorized

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -48,11 +48,15 @@ console.log("user: ", user?.email, "allowed: ", ALLOWED_EMAIL);
     <div>
       {user ? (
         <>
-          
+          {user.email === ALLOWED_EMAIL ? (
             <span className="mx-[2rem] text-white bg-gray-700 py-2 px-4 rounded-md">
               Hi, {user.displayName || user.email}
             </span>
-          
+          ) : (
+            <span className="mx-[2rem] text-yellow-500 bg-gray-700 py-2 px-4 rounded-md">
+              Unauthorized User
+            </span>
+          )}
           <button
             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
             onClick={logout}
