7eae65f02eaa167b9f1551b5506730e546c3ead9
aayalapalacin <alexander.ayala.palacin@gmail.com>
2025-11-09 22:10:14 -0500

    updated fetch.ts and admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,6 +7,7 @@ import { firestore } from "@/lib/firebase";
 import { collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
 import Expenses from "@/components/ExpenseList";
 import { PayOut } from "@/components/PayOut";
+import { fetchCategories,fetchIncomes } from "@/assets/fetch";
 
 interface Expense {
   title: string;
@@ -36,53 +37,11 @@ export default function Admin() {
 
 
   useEffect(() => {
-    const fetchIncomes = async () => {
-      try {
-        const usersCollection = collection(firestore, 'users');
-        const usersSnapshot = await getDocs(usersCollection);
+  
+  
 
-        let alexIncome = "0";
-        let linaIncome = "0";
-
-        usersSnapshot.forEach((doc) => {
-          const userData = doc.data();
-           console.log(doc.id,"userData")
-          if (userData.name === "Alex") {
-            setAlexDocId(doc.id)
-            alexIncome = userData.income ? userData.income.toString() : "0";
-          } else if (userData.name === "Lina") {
-            setLinaDocId(doc.id)
-            linaIncome = userData.income ? userData.income.toString() : "0";
-          }
-        });
-
-        setIncome({ alex: alexIncome, lina: linaIncome });
-
-      } catch (error) {
-        console.error("Error fetching incomes:", error);
-      }
-    };
-    const fetchCategories = async () => {
-      try {
-        const categoriesCollection = collection(firestore, "categories");
-        const categoriesSnapshot = await getDocs(categoriesCollection);
-
-        const categoryList = categoriesSnapshot.docs.map((doc) => {
-          const data = doc.data();
-          return {
-            id: doc.id,
-            name: data.name || "",
-          } as Category;
-        });
-
-        setCategories(categoryList);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
-    };
-
-    fetchCategories();
-    fetchIncomes();
+    fetchCategories(setCategories);
+    fetchIncomes(setAlexDocId,setLinaDocId,setIncome);
   }, []);
 
   const handleIncomeChange = (name: "alex" | "lina", value: string) => {
--- src/assets/fetch.ts
+++ src/assets/fetch.ts
@@ -1,4 +1,4 @@
-import { Expense } from "./types";
+import { Expense, Category } from "./types";
 import { collection, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
 import { firestore } from "@/lib/firebase";
 
@@ -23,5 +23,54 @@ export const fetchExpenses = async (  setter: React.Dispatch<React.SetStateActio
         setter(expensesList);
       } catch (error) {
         console.error("Error fetching expenses:", error);
+      }
+    };
+
+  export const fetchCategories = async (setter: React.Dispatch<React.SetStateAction<Category[]>>) => {
+      try {
+        const categoriesCollection = collection(firestore, "categories");
+        const categoriesSnapshot = await getDocs(categoriesCollection);
+
+        const categoryList = categoriesSnapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            name: data.name || "",
+          } as Category;
+        });
+
+        setter(categoryList);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+      
+    export const fetchIncomes = async (
+      setAlexDocId: React.Dispatch<React.SetStateAction<string | null>>,
+      setLinaDocId: React.Dispatch<React.SetStateAction<string | null>>,
+      setIncome:React.Dispatch<React.SetStateAction<{ alex: string; lina: string }>>) => {
+      try {
+        const usersCollection = collection(firestore, 'users');
+        const usersSnapshot = await getDocs(usersCollection);
+
+        let alexIncome = "0";
+        let linaIncome = "0";
+
+        usersSnapshot.forEach((doc) => {
+          const userData = doc.data();
+           console.log(doc.id,"userData")
+          if (userData.name === "Alex") {
+            setAlexDocId(doc.id)
+            alexIncome = userData.income ? userData.income.toString() : "0";
+          } else if (userData.name === "Lina") {
+            setLinaDocId(doc.id)
+            linaIncome = userData.income ? userData.income.toString() : "0";
+          }
+        });
+
+        setIncome({ alex: alexIncome, lina: linaIncome });
+
+      } catch (error) {
+        console.error("Error fetching incomes:", error);
       }
     };
